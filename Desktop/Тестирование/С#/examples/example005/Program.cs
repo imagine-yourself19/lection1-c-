345a8b6f99bda58e9a9c55f038936906f0202e1c
Lena <imagine.youeself19@gmail.com>
2022-06-22 13:08:41 +0300

    create example005

---
+++ Desktop/Тестирование/С#/examples/example005/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "вадим")
+{
+    Console.WriteLine("Здравствуй, мой любимый Вадим");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
